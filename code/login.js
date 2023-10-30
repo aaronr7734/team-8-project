e4ab3499954f00435ed81b6c92d31a8c970872bb
Aaron <ramirezja16@gmail.com>
2023-10-29 18:55:14 -0700

    Added DOMContentLoaded to both javascript files to ensure their respective html pages load fully before they execute.

--- code/login.js
+++ code/login.js
@@ -1,29 +1,30 @@
 // I only know the very basics of javascript
 // So I'm not sure if this is the best way to do this.
+document.addEventListener("DOMContentLoaded", function() {
+    const loginFormEl = document.querySelector('.login_process.php'); 
 
-const loginFormEl = document.querySelector('.login_process.php'); 
+    loginFormEl.addEventListener('submit', event => {
+        event.preventDefault(); 
 
-loginFormEl.addEventListener('submit', event => {
-    event.preventDefault(); 
+        const formData = new FormData(loginFormEl);
+        const loginData = Object.fromEntries(formData);
 
-    const formData = new FormData(loginFormEl);
-    const loginData = Object.fromEntries(formData);
-
-    fetch('https://studentdiscountz.org/api/login', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(loginData)
-    }).then(res => res.json())
-      .then(data => {
-        if (data.token) {
-            // Redirect to home page.
-            window.location.href = "https://studentdiscountz.org/";
-        } else {
-            // Show error // this is the part I'm not sure is safe.
-            alert(JSON.stringify(data));
-        }
-    })
-    .catch(error => console.log(error));
+        fetch('https://studentdiscountz.org/api/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(loginData)
+        }).then(res => res.json())
+        .then(data => {
+            if (data.token) {
+                // Redirect to home page.
+                window.location.href = "https://studentdiscountz.org/";
+            } else {
+                // Show error // this is the part I'm not sure is safe.
+                alert(JSON.stringify(data));
+            }
+        })
+        .catch(error => console.log(error));
+    });
 });
