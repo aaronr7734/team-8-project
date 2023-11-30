4eed098fb64bf106bf9a4c3f42f4808d9bbe51f6
cjj01 <ceannajarrett@gmail.com>
2023-11-29 19:20:33 -0700

    new discounts js and jquery

--- code/DiscountsJavaScipt.js
+++ code/DiscountsJavaScipt.js
@@ -1,23 +1,13 @@
-fetch('https://studentdiscountz.org/api/Discount/')
-   .then((response) =>{
+$.get('https://studentdiscountz.org/api/Discounts'), function(name,description,business_name, url) {
+  alert(name + "\n" + description + "\n" + business_name + "\n" + url + "\n" );
+}
 
-    return response.json();
-   })
-   .then(name => 
-    //fetch the name
-    document.getElementById("name").innerHTML = name)
-    //fetch the businessname
-    .then(businessName => document.getElementById("business_name").innerHTML = businessName)
-    //fetch the description
-    .then(descriptionText => document.getElementById("description").innerHTML = descriptionText)
-    //fetch the url
-    .then(urlText => document.getElementById("url").innerHTML = urlText)
+// update the html to class
+$('nameD')
+.fail(function(){
+  console.error('Error getting data');
+});  
 
-    
-   .catch(function(error) {
-     console.log(error)
-   });
    
 
     
-   
