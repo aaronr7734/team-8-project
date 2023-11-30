4f18a76ae749ea4a50e81abeca81c7127ce9b0c9
cjj01 <ceannajarrett@gmail.com>
2023-11-30 16:29:15 -0700

    Adding on the post of the js

--- code/discounts.js
+++ code/discounts.js
@@ -2,3 +2,15 @@ $.get("https://studentdiscountz.org/api/discounts/",
 function(data, status) {
     console.log(data, status);
 })
+$.post("https://studentdiscountz.org/api/discounts/",
+{suggest: txt}, function(name){
+    $("nameJs").html(name);
+})
+$.post("https://studentdiscountz.org/api/discounts/",
+{suggest: txt}, function(description){
+    $("descriptionJs").html(description);
+})
+$.post("https://studentdiscountz.org/api/discounts/",
+{suggest: txt}, function(url){
+    $("urlJs").html(url);
+})
