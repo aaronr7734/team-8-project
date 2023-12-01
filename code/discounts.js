7a733decc7eb98ef3cf75d8146f8209cb6e2e2e1
cjj01 <ceannajarrett@gmail.com>
2023-12-01 08:44:02 -0700

    add the id

--- code/discounts.js
+++ code/discounts.js
@@ -1,27 +1,24 @@
 $(function (){
     var $discounts = $('#discounts')
-    var $discountsId = 1;
+    var discountsId = 1;
     //var $name = $('#nameID')
    // var $locationJs = $('#locationJs')
     //var $urlJs = $('#urlJs')
     $.ajax({
         type: 'GET',
         url: 'https://studentdiscountz.org/api/discounts/',
-        data: {id : discountId},
-        success: function(data){
-            if(data.length > 0){
-                var item = data[0];
-                $discounts.append('<p>' + item.name + '</p>');
-                $discounts.append('<p>' + item.description + '</p>');
-                $discounts.append('<p>' + item.url + '</p>');
-                $discounts.append('<p>' + item.location + '</p>');
-    
-            }
-            else{
-                $discounts.append('<p> Nothing to show </p>');
-            }
-        
-        
+        success: function(discounts){
+        var selectedDiscounts = discounts.find(function (discount){
+            return discount.id  === discountsId;
+        })
+        if(selectedDiscounts){
+            $discounts.append('<p>' + description.name + '</p>');
+            $discounts.append('<p>' + description.description + '</p>');
+            $discounts.append('<p>' + description.url + '</p>');
+            $discounts.append('<p>' + description.location + '</p>');
+
+        }
+       
         }
 
     });  
