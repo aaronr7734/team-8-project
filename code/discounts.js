a0f32e4cdd52e250a7d0cbeb834753e813795e16
cjj01 <ceannajarrett@gmail.com>
2023-12-01 11:42:45 -0700

    adding the class to js

--- code/discounts.js
+++ code/discounts.js
@@ -13,11 +13,11 @@ $(function (){
             return discount.id  === discountsId;
         })
         if(selectedDiscounts){
-            $discounts.append('<p>' + selectedDiscounts.name + '</p>');
-            $discounts.append('<p>' + selectedDiscounts.description + '</p>');
+            $discounts.append('<p class="deal-description">' + selectedDiscounts.name + '</p>');
+            $discounts.append('<p class="deal-description">' + selectedDiscounts.description + '</p>');
              //$discounts.append('<p>' + selectedDiscounts.url + '</p>');
-            $discounts.append('<p> <a href="' + selectedDiscounts.url + '" target="_blank"> View Deal </a></p>' );
-            $discounts.append('<p>' + selectedDiscounts.location + '</p>');
+            $discounts.append('<p class="btn btn-primary "> <a href="' + selectedDiscounts.url + '" target="_blank"> View Deal </a></p>' );
+            $discounts.append('<p class="deal-description">' + selectedDiscounts.location + '</p>');
 
         }
        
