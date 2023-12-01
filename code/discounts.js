0f1c6214d11616742cc84408bf0ccea8cc8bb2bc
cjj01 <ceannajarrett@gmail.com>
2023-12-01 00:44:51 -0700

    trying code

--- code/discounts.js
+++ code/discounts.js
@@ -1,37 +1,33 @@
 $(function (){
     var $discounts = $('#discounts')
     var $name = $('#nameID')
-    var $locationJs = $('locationJs')
-    var urlJs = $('urljs')
+    var $locationJs = $('#locationJs')
+    var urlJs = $('#urljs')
     $.ajax({
         type: 'GET',
         url: 'https://studentdiscountz.org/api/discounts/',
         success: function(name){
             $.each(name, function(data){
-                $discounts.append('<p>' + data.name + '</p>');
+                $name.append('<p>' + data.name + '</p>');
             });
 
         },
         success: function(discounts){
             $.each(discounts, function(data){
-                $discounts.append('<p>' + data.name + '</p>');
                 $discounts.append('<p>' +description.description + '</p>');
-                $discounts.append('<p>' + description.location + '</p>');
-                $discounts.append('<a href=">' + description.url + '" class="btn btn-primary" >' + '</a>');
-                
 
             });
 
         },
         success: function(locationJs){
             $.each(locationJs, function(data){
-                $discounts.append('<p>' + description.location + '</p>');
+                $locationJs.append('<p>' + description.location + '</p>');
             });
             
         },
         success: function(urlJs){
             $.each(discounts, function(data){
-                 $discounts.append('<a href=">' + description.url + '" class="btn btn-primary" >' + '</a>');
+                 $urlJs.append('<a href=">' + description.url + '" class="btn btn-primary" >' + '</a>');
                     
     
                 });
