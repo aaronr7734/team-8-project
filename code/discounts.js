d14e08befc720de2c1b5a5156b28e06d3745fa8e
cjj01 <ceannajarrett@gmail.com>
2023-12-01 08:11:45 -0700

    try this

--- code/discounts.js
+++ code/discounts.js
@@ -6,12 +6,12 @@ $(function (){
     $.ajax({
         type: 'GET',
         url: 'https://studentdiscountz.org/api/discounts/',
-        success: function(discounts){
-        $.column(discounts, function(index, description){
-            $discounts.append('<p>' + description.name + '</p>');
-            $discounts.append('<p>' +description.description + '</p>');
-            $discounts.append('<p>' + description.url + '</p>');
-            $discounts.append('<p>' + description.location + '</p>');
+        success: function(data){
+        $.each(discounts, function(index, item){
+            $discounts.append('<p>' + item.name + '</p>');
+            $discounts.append('<p>' + item.description + '</p>');
+            $discounts.append('<p>' + item.url + '</p>');
+            $discounts.append('<p>' + item.location + '</p>');
 
 
          });
