c925060e48ac2c692605dc0d20218cf7cd6f890f
cjj01 <ceannajarrett@gmail.com>
2023-12-01 07:12:33 -0700

    trying old code new

--- code/discounts.js
+++ code/discounts.js
@@ -7,11 +7,11 @@ $(function (){
         type: 'GET',
         url: 'https://studentdiscountz.org/api/discounts/',
         success: function(discounts){
-        $.each(discounts, function(data){
-            $discounts.append('<p>' + data.name + '</p>');
-            $discounts.append('<p>' +data.description + '</p>');
-            $discounts.append('<p>' + data.url + '</p>');
-            $discounts.append('<p>' + data.location + '</p>');
+        $.each(discounts, function(name, description, url, business_name, location){
+            $discounts.append('<p>' + name.name + '</p>');
+            $discounts.append('<p>' +description.description + '</p>');
+            $discounts.append('<p>' + url.url + '</p>');
+            $discounts.append('<p>' + location.location + '</p>');
 
 
         });
