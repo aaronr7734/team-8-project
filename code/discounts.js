67f9666e9c814d41221aa5d5b041e551432ee846
cjj01 <ceannajarrett@gmail.com>
2023-12-01 00:52:39 -0700

    trying code

--- code/discounts.js
+++ code/discounts.js
@@ -14,20 +14,20 @@ $(function (){
         },
         success: function(discounts){
             $.each(discounts, function(data){
-                $discounts.append('<p>' +description.description + '</p>');
+                $discounts.append('<p>' +data.description + '</p>');
 
             });
 
         },
         success: function(locationJs){
             $.each(locationJs, function(data){
-                $locationJs.append('<p>' + description.location + '</p>');
+                $locationJs.append('<p>' + data.location + '</p>');
             });
             
         },
         success: function(urlJs){
             $.each(discounts, function(data){
-                 $urlJs.append('<a href=">' + description.url + '" class="btn btn-primary" >' + '</a>');
+                 $urlJs.append('<a href=">' + date.url + '" class="btn btn-primary" >' + '</a>');
                     
     
                 });
