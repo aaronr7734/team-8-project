8c8b6bc6aa15bfa2181ed7bf039f30416079ae94
cjj01 <ceannajarrett@gmail.com>
2023-12-01 00:32:31 -0700

    new js

--- code/discounts.js
+++ code/discounts.js
@@ -1,24 +1,23 @@
 $(function (){
-    var $discounts = $('#discounts')
+    var $discounts = $('#discounts');
+
     $.ajax({
         type: 'GET',
         url: 'https://studentdiscountz.org/api/discounts/',
-        success: function(discounts){
-            $.each(discounts, function(name, description, url, business_name, location){
-                $discounts.append('<p>' + description.name + '</p>');
-                $discounts.append('<p>' +description.description + '</p>');
-                $discounts.append('<p>' + description.location + '</p>');
-                $discounts.append('<a href=">' + description.url + '" class="btn btn-primary" >' + '</a>');
-                
+        success: function(data) {
+            $.each(data, function(index, discount) {
+                // Append name, description, location, and a link for each discount
+                $discounts.append('<p><strong>Name:</strong> ' + discount.name + '</p>');
+                $discounts.append('<p><strong>Description:</strong> ' + discount.description + '</p>');
+                $discounts.append('<p><strong>Location:</strong> ' + discount.location + '</p>');
+                $discounts.append('<a href="' + discount.url + '" class="btn btn-primary">Visit Website</a>');
 
+                // Add a horizontal line for visual separation between discounts
+                $discounts.append('<hr>');
             });
-
+        },
+        error: function(error) {
+            console.log('Error loading discounts:', error);
         }
     });
 });
-
-
-
-
-
-
