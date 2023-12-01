4618f23473c222e1241f35374a6349fd67eadaca
cjj01 <ceannajarrett@gmail.com>
2023-11-30 22:31:03 -0700

    try the discounts

--- code/discounts.js
+++ code/discounts.js
@@ -1,16 +1,12 @@
-$.get("https://studentdiscountz.org/api/discounts/", 
-function(data, status) {
-    console.log(data, status);
-})
-$.post("https://studentdiscountz.org/api/discounts/",
-{suggest: txt}, function(name){
-    $("nameJs").html(name);
-})
-$.post("https://studentdiscountz.org/api/discounts/",
-{suggest: txt}, function(description){
-    $("descriptionJs").html(description);
-})
-$.post("https://studentdiscountz.org/api/discounts/",
-{suggest: txt}, function(url){
-    $("urlJs").html(url);
-})
+$(document).ready(function () {
+    $.get("https://studentdiscountz.org/api/discounts/", 
+    function(data, status) {
+        console.log(data, status);
+    });
+    $.post("https://studentdiscountz.org/api/discounts/", {user: "la", body: "0"}, 
+    function(data, status) {
+        console.log(data, status); 
+    });
+});
+
+
