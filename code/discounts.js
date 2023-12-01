756b5ce2c5f5ff17b702c7be17589582f8cbdf79
cjj01 <ceannajarrett@gmail.com>
2023-12-01 00:38:06 -0700

    something new

--- code/discounts.js
+++ code/discounts.js
@@ -1,23 +1,48 @@
 $(function (){
-    var $discounts = $('#discounts');
-
+    var $discounts = $('#discounts')
+    var $name = $('#nameID')
+    var $locationJs = $('locationJs')
+    var urlJs = $('urljs')
     $.ajax({
         type: 'GET',
         url: 'https://studentdiscountz.org/api/discounts/',
-        success: function(data) {
-            $.each(data, function(index, discount) {
-                // Append name, description, location, and a link for each discount
-                $discounts.append('<p><strong>Name:</strong> ' + discount.name + '</p>');
-                $discounts.append('<p><strong>Description:</strong> ' + discount.description + '</p>');
-                $discounts.append('<p><strong>Location:</strong> ' + discount.location + '</p>');
-                $discounts.append('<a href="' + discount.url + '" class="btn btn-primary">Visit Website</a>');
-
-                // Add a horizontal line for visual separation between discounts
-                $discounts.append('<hr>');
+        success: function(name){
+            $.each(name, function(data){
+                $discounts.append('<p>' + data.name + '</p>');
             });
+
         },
-        error: function(error) {
-            console.log('Error loading discounts:', error);
+        success: function(discounts){
+            $.each(discounts, function(data){
+                $discounts.append('<p>' + data.name + '</p>');
+                $discounts.append('<p>' +description.description + '</p>');
+                $discounts.append('<p>' + description.location + '</p>');
+                $discounts.append('<a href=">' + description.url + '" class="btn btn-primary" >' + '</a>');
+                
+
+            });
+
+        },
+        success: function(locationJs){
+            $.each(locationJs, function(data){
+                $discounts.append('<p>' + description.location + '</p>');
+            });
+            
+        },
+        success: function(urlJs){
+            $.each(discounts, function(data){
+                 $discounts.append('<a href=">' + description.url + '" class="btn btn-primary" >' + '</a>');
+                    
+    
+                });
+
         }
+
     });
 });
+
+
+
+
+
+
