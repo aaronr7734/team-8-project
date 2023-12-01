f02540e3dac5b798afb92785d02377e69288eec9
cjj01 <ceannajarrett@gmail.com>
2023-12-01 08:21:07 -0700

    try this

--- code/discounts.js
+++ code/discounts.js
@@ -1,20 +1,26 @@
 $(function (){
     var $discounts = $('#discounts')
+    var $discountsId = 1;
     //var $name = $('#nameID')
    // var $locationJs = $('#locationJs')
     //var $urlJs = $('#urlJs')
     $.ajax({
         type: 'GET',
         url: 'https://studentdiscountz.org/api/discounts/',
+        data: {id : discountId},
         success: function(data){
-        $.each(data, function(index, item){
-            $discounts.append('<p>' + item.name + '</p>');
-            $discounts.append('<p>' + item.description + '</p>');
-            $discounts.append('<p>' + item.url + '</p>');
-            $discounts.append('<p>' + item.location + '</p>');
-
-
-         });
+            if(data.length > 0){
+                var item = data[0];
+                $discounts.append('<p>' + item.name + '</p>');
+                $discounts.append('<p>' + item.description + '</p>');
+                $discounts.append('<p>' + item.url + '</p>');
+                $discounts.append('<p>' + item.location + '</p>');
+    
+            }
+            else{
+                $discounts.append('<p> Nothing to show </p>');
+            }
+        
         
         }
 
