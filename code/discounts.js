09c10cfaa5220cd7d8f708db9705ba549ee3d8e9
cjj01 <ceannajarrett@gmail.com>
2023-12-01 09:01:14 -0700

    add the array new

--- code/discounts.js
+++ code/discounts.js
@@ -1,36 +1,27 @@
-$(function (){
-    var $discounts = $('#discounts')
-   
-    var discountsIdShow = [1,2,3] ;
-    //var $name = $('#nameID')
-   // var $locationJs = $('#locationJs')
-    //var $urlJs = $('#urlJs')
+$(function () {
+    var $discounts = $('#discounts');
+    var discountIdsToShow = [1, 2, 3]; // Change these IDs to the ones you want to display
+
     $.ajax({
         type: 'GET',
         url: 'https://studentdiscountz.org/api/discounts/',
-        success: function(discounts){
+        success: function (discounts) {
+            // Loop through each ID in the array
             discountIdsToShow.forEach(function (discountId) {
                 // Find the discount with the current ID
                 var selectedDiscount = discounts.find(function (discount) {
                     return discount.id === discountId;
                 });
-            if(selectedDiscounts){
-                $discounts.append('<p>' + selectedDiscounts.name + '</p>');
-                $discounts.append('<p>' + selectedDiscounts.description + '</p>');
-                $discounts.append('<p>' + selectedDiscounts.url + '</p>');
-                $discounts.append('<p>' + selectedDiscounts.location + '</p>');
 
-            }
-       
-        });
-    }
+                if (selectedDiscount) {
+                    // Display the selected discount
+                    $discounts.append('<p> ' + selectedDiscount.name + '</p>');
+                    $discounts.append('<p> ' + selectedDiscount.description + '</p>');
+                    $discounts.append('<p> ' + selectedDiscount.url + '</p>');
+                    $discounts.append('<p>' + selectedDiscount.location + '</p>');
+                    $discounts.append('<hr>'); // Add a horizontal line between discounts
+                }
+            });
+        }
     });
-
-});  
-    
-
-
-
-
-
-
+});
