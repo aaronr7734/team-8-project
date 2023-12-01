9c1382e5eca41d8c906e4e35c9ccbe3be63bf062
cjj01 <ceannajarrett@gmail.com>
2023-12-01 07:09:08 -0700

    trying old code

--- code/discounts.js
+++ code/discounts.js
@@ -1,38 +1,21 @@
 $(function (){
     var $discounts = $('#discounts')
-    var $name = $('#nameID')
-    var $locationJs = $('#locationJs')
-    var $urlJs = $('#urlJs')
+   //var $name = $('#nameID')
+   // var $locationJs = $('#locationJs')
+    //var $urlJs = $('#urlJs')
     $.ajax({
         type: 'GET',
         url: 'https://studentdiscountz.org/api/discounts/',
-        success: function(name){
-            $.each(name, function(data){
-                $name.append('<p>' + data.name + '</p>');
-            });
-
-        },
         success: function(discounts){
-            $.each(discounts, function(data){
-                $discounts.append('<p>' +data.description + '</p>');
+        $.each(discounts, function(name, description, url, business_name, location){
+            $discounts.append('<p>' + discounts.name + '</p>');
+            $discounts.append('<p>' +discounts.description + '</p>');
+            $discounts.append('<p>' + discounts.url + '</p>');
+            $discounts.append('<p>' + discounts.location + '</p>');
 
-            });
 
-        },
-        success: function(locationJs){
-            $.each(locationJs, function(data){
-                $locationJs.append('<p>' + data.location + '</p>');
-            });
-            
-        },
-        success: function(urlJs){
-            $.each(discounts, function(data){
-                 $urlJs.append('<p>' + date.url + '</p>');
-                    
-    
-                });
-
-        }
+        });
+    }
 
     });
 });
