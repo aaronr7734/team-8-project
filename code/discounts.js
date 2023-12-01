437cb5a67afe256f8661f2c1a7b03f79a4fd4c07
cjj01 <ceannajarrett@gmail.com>
2023-11-30 23:22:49 -0700

    Try this new code new

--- code/discounts.js
+++ code/discounts.js
@@ -5,7 +5,7 @@ $(function (){
         url: 'https://studentdiscountz.org/api/discounts/',
         success: function(discounts){
             $.each(discounts, function(name, description, url, business_name, location){
-                $discounts.append('<p>' + discounts.name + '</p>');
+                $discounts.append('<p>' + description.name + '</p>');
                 $discounts.append('<p>' +discounts.description + '</p>');
                 $discounts.append('<p>' + discounts.url + '</p>');
                 $discounts.append('<p>' + discounts.location + '</p>');
