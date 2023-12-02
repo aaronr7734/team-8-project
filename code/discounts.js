8c7a3ec86af52b4abdd49c4990f77f21da3f15dc
cjj01 <ceannajarrett@gmail.com>
2023-12-02 00:13:57 -0700

    try the image and the first discount"

--- code/discounts.js
+++ code/discounts.js
@@ -13,9 +13,10 @@ $(function (){
             return discount.id  === discountsId;
         })
         if(selectedDiscounts){
+            $discounts.append('<p class="deal-description> <img src="' + selectedDiscounts.image+ '" alt="gadget-image" class="img-fluid"> </p>');
             $discounts.append('<p class="deal-description">' + selectedDiscounts.name + '</p>');
             $discounts.append('<p class="deal-description">' + selectedDiscounts.description + '</p>');
-             //$discounts.append('<p>' + selectedDiscounts.url + '</p>');
+            
             $discounts.append('<p class="btn btn-primary "> <a href="' + selectedDiscounts.url + '" target="_blank"> View Deal </a></p>' );
             $discounts.append('<p class="deal-description">' + selectedDiscounts.location + '</p>');
 
