9624d1ddf7b6cd19deabb120d46da6538bb922ec
jmo366 <jmo366@nau.edu>
2023-12-02 13:32:29 -0700

    signup updated

--- code/signUp.js
+++ code/signUp.js
@@ -1,47 +1,46 @@
 function signUpForm() {
-    const firstName = document.getElementById('firstname').value;
-    const lastName = document.getElementById('lastname').value;
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
-    const confirmPassword = document.getElementById('confirmPassword').value;
+  const first_name = document.getElementById('first_name').value;
+  const last_name = document.getElementById('last_name').value;
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+  const confirmPassword = document.getElementById('confirmPassword').value;
 
-    if (password !== confirmPassword) {
-        alert('Passwords do not match. Please try again');
-        return;
-    }
+  if (password !== confirmPassword) {
+    alert('Passwords do not match. Please try again');
+    return;
+  }
 
-    const userData = {
-        firstName: firstName,
-        lastName: lastName,
-        email: email,
-        password: password,
-    };
+  const userData = {
+    first_name,
+    last_name,
+    email,
+    password,
+  };
 
-	fetch('https://studentdiscountz.org/api/register/', {
-	    method: 'POST',
-	    headers: {
-	        'Content-Type': 'application/json'
-	    },
-	    body: JSON.stringify(userData),
-	})
-	    .then(response => {
-	        if (!response.ok) {
-	            throw new Error('Network response was not ok');
-	        }
-	        return response.json();
-	    })
-	    .then(data => {
-	        if (data.success) {
-	            alert("Account Successfully Created!");
-	            console.log("Test:", data);
-	            window.location.href = 'index.html';
-	        } else {
-	            console.error('Error: ', data.error);
-	        }
-	    })
-	    .catch(error => {
-	        console.error('Fetch error:', error);
-	        alert('Sign Up Incomplete');
-	    });
-	}
+console.log(JSON.stringify(userData));
+
+  fetch('https://studentdiscountz.org/api/register/', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(userData),
+  })
+    .then(response => response.json())
+    .then(data => {
+       alert('Account Successfully Created!');
+    })
+    .catch(error => {
+      alert('Sign Up Incomplete');
+    });
+}
+
+const form = document.getElementById('Signup');
+form.addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  setTimeout(function () {
+    window.location.href = 'index.html';
+  }, 2000);
+});
 
