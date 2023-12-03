76d8f50f3b1a8c67fe482e6b50c77da88117b6e3
plii32 <131200510+plii32@users.noreply.github.com>
2023-12-03 16:00:42 -0700

    Update ContactUs_test.py

--- deliverables/acceptance_tests/ContactUs_test.py
+++ deliverables/acceptance_tests/ContactUs_test.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-# Replace 'path/to/msedgedriver' with the absolute path to your downloaded Microsoft Edge Driver executable
+# The path to my downloaded Microsoft Edge Driver executable
 edge_driver_path = 'Documents/CS386/myenv/Scripts/activate/edgedriver_win64/msedgedriver.exe'
 
 # Create EdgeOptions object and set the executable path
@@ -20,13 +20,15 @@ driver.get('https://www.studentdiscountz.org')
 contact_us_link = WebDriverWait(driver, 10).until(
     EC.element_to_be_clickable((By.XPATH, '//div[@class="footer"]/a[@href="ContactUsPage.html"]'))
 )
+# Click on the contact us link
 contact_us_link.click()
 
+# Find the GitHub link
 github_link = WebDriverWait(driver, 10).until(
     EC.element_to_be_clickable((By.XPATH, '//div[@class="mission-statement"]//a[@href="https://github.com/aaronr7734/team-8-project"]'))
 )
 
-# click on github link
+# Click on Github link
 github_link.click()
 
 # Close the browser
