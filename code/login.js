4e568946a9ef4f210ac366abb7e1635ec1605ec2
jmo366 <jmo366@nau.edu>
2023-12-02 17:45:17 -0700

    login js updated

--- code/login.js
+++ code/login.js
@@ -1,37 +1,39 @@
 function loginForm() {
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value; 
+  const username = document.getElementById('email').value
+  const email = document.getElementById('email').value
+  const password = document.getElementById('password').value;
 
-    const userData = {
-        username: username,
-        password: password,
-    };
+  const userData = {
+    username,
+    email,
+    password,
+  };
 
-        fetch('https://studentdiscountz.org/api/register/', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(userData),
-        })
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Network response was not ok');
-                }
-                return response.json();
-            })
-            .then(data => {
-                if (data.success) {
-                    alert("Account Successfully Logged in!");
-                    window.location.href = 'index.html';
-                } else {
-                    alert('Error. ' + data.message);
-                    console.error('Error: ', data.error);
-                }
-            })
-            .catch(error => {
-                console.error('Fetch error:', error);
-                alert('Login Up Incomplete');
-            });
-        }
-n
+console.log(JSON.stringify(userData));
+
+  fetch('https://studentdiscountz.org/api/login/', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(userData),
+  })
+    .then(response => response.json())
+    .then(data => {
+       alert('Welcome Back!');
+       window.location.href = 'index.html';
+    })
+    .catch(error => {
+      alert('Login Up Incomplete');
+    });
+
+const form = document.getElementById('login');
+form.addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  setTimeout(function () {
+    window.location.href = 'index.html';
+  }, 2000);
+});
+
+}
